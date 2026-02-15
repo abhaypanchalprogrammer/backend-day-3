22a0857a22e32918b6ee7d46faf94440050eed57
abhaypanchalprogrammer <abhaypanchal2525@gmail.com>
2026-02-15 11:03:49 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ app.post("/notes", (req, res) => {
   console.log(req.body);
   notes.push(req.body);
   res.send("note created");
+  console.log(notes);
 });
 app.get("/notes", (req, res) => {
   res.send(notes);
